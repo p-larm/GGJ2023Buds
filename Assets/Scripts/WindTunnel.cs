1a60c1cb2fd84891cbf04ad03bfc8f6923cc4ba2
p-larm <56834903+p-larm@users.noreply.github.com>
2023-02-04 13:42:43 -0500

    Wind System & New Root Connections
    I finished the wind system, now I will begin creating the new roots to make connections to.

--- Assets/Scripts/WindTunnel.cs
+++ Assets/Scripts/WindTunnel.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WindTunnel : MonoBehaviour
 {
+    [Header("Wind Force/Direction")]
     [SerializeField]
     private float windForce;
     [SerializeField] [Range(0,1)]
@@ -11,11 +12,18 @@ public class WindTunnel : MonoBehaviour
     [SerializeField]
     private List<Vector2> windDirections;
     private Vector2 targetDirection;
+    [SerializeField]
     private Vector2 currentDirection;
-    private float xDir;
-    private float xDirInc;
-    private float yDir;
-    private float yDirInc;
+
+    [Header("Particle Effects")]
+    [SerializeField]
+    private ParticleSystem loopEffect;
+    [SerializeField]
+    private ParticleSystem straightEffect;
+    [SerializeField]
+    private float emissionRateMult;
+    private BoxCollider boxCollider;
+
     private bool active;
 
     [SerializeField]
@@ -26,11 +34,15 @@ public class WindTunnel : MonoBehaviour
     {
         budList = new List<PlayerMovement>();
         active = true;
+        boxCollider = GetComponent<BoxCollider>();
         StartCoroutine(WindDirection());
     }
 
     private void Update() {
         Vector3 force = new Vector3(currentDirection.x, 0, currentDirection.y) * windForce;
+        Debug.Log(force.ToString());
+        HandleEffects(force);
+
         foreach(PlayerMovement bud in budList) {
             bud.AddForce(force);
         }
@@ -59,7 +71,16 @@ public class WindTunnel : MonoBehaviour
         while(active) {
             foreach(Vector2 direction in windDirections) {
                 targetDirection = direction;
-                while(Vector2.Angle(currentDirection, targetDirection) > 1f) {
+                var loopShape = loopEffect.shape;
+                var straightShape = straightEffect.shape;
+                if(direction.x != 0) {
+                    loopShape.scale = new Vector3(boxCollider.size.x, boxCollider.size.z, boxCollider.size.z);
+                    straightShape.scale = new Vector3(boxCollider.size.x, boxCollider.size.z, boxCollider.size.z);
+                } else {
+                    loopShape.scale = new Vector3(boxCollider.size.z, boxCollider.size.x, boxCollider.size.x);
+                    straightShape.scale = new Vector3(boxCollider.size.z, boxCollider.size.x, boxCollider.size.x);
+                }
+                while(Vector2.Angle(currentDirection, targetDirection) > 5f) {
                     currentDirection = Vector2.Lerp(currentDirection, targetDirection, windChangeAmt);
                     yield return null;
                 }
@@ -67,5 +88,31 @@ public class WindTunnel : MonoBehaviour
                 yield return new WaitForSeconds(5);
             }
         }
+    }
+
+    private void HandleEffects(Vector3 force) {
+        // Handle Direction
+        Vector3 direction = Vector3.Normalize(force);
+
+        loopEffect.gameObject.transform.right = Vector3.Lerp(loopEffect.gameObject.transform.right, direction, 0.1f);
+        loopEffect.gameObject.transform.Rotate(new Vector3(90, 0, 0), Space.Self);
+
+/*             if(force.x < 0) {
+                loopEffect.gameObject.transform.localScale *= -1;
+                straightEffect.gameObject.transform.localScale *= 1;
+            } else {
+                loopEffect.gameObject.transform.localScale = -1f;
+                straightEffect.gameObject.transform.localScale = -1f;
+            } */
+
+        straightEffect.gameObject.transform.eulerAngles = loopEffect.gameObject.transform.eulerAngles;
+
+
+        // Handle Speed
+        var loopEmission = loopEffect.emission;
+        var straightEmission = straightEffect.emission;
+        float emissionRate = Mathf.Abs(force.magnitude) * emissionRateMult;
+        loopEmission.rateOverTime = emissionRate;
+        straightEmission.rateOverTime = emissionRate;
     }
 }
